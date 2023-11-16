c133b18b96b40b69be79757d685510c6f958f57d
Alexander Ho <alexyho9@gmail.com>
2023-11-16 01:18:30 -0800

    problem 001

--- problems/problem_001.py
+++ problems/problem_001.py
@@ -9,4 +9,10 @@
 # problem to get a good feel for how to solve it.
 
 def minimum_value(value1, value2):
-    pass
+    if value1 < value2:
+        return value1
+    return value2
+
+
+print(minimum_value(5, 9))
+print(minimum_value(5, -3))
