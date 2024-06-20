d0a307d81bcb574a95fed07a06d46711e8bec8a6
Alexander Ho <alexyho9@gmail.com>
2024-06-20 16:35:06 -0700

    Completed problem 016

--- problems/problem_016.py
+++ problems/problem_016.py
@@ -3,4 +3,24 @@
 # make sure they're between 0 and 10, inclusive.
 
 def is_inside_bounds(x, y):
-    pass
+    for num in x:
+        if num > 10 or num < 0:
+            return False
+    for num in y:
+        if num > 10 or num < 0:
+            return False
+    return True
+
+
+x1 = (4, 7)
+y1 = (10, 2)
+
+x2 = (9, 3)
+y2 = (12, 1)
+
+x3 = (9, 3)
+y3 = (-6, 1)
+
+print(is_inside_bounds(x1, y1))
+print(is_inside_bounds(x2, y2))
+print(is_inside_bounds(x3, y3))
